69c91a46c08922e5b236487ff8c075948f93b533
James Hughes <james.a.hughes@gmail.com>
2018-07-22 14:21:53 -0500

    Add components to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React from 'react';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello!</h1>
+        <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   }
 };
+
 
 export default App;
