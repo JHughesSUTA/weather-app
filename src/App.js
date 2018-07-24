b7bf62a288e3dee6280f98c4929e31d607bb532d
James Hughes <james.a.hughes@gmail.com>
2018-07-23 21:31:43 -0500

    Display error message when blank form is submitted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
@@ -26,16 +26,27 @@ class App extends React.Component {
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
     const data = await api_call.json();
 
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""  
-     });
+    if (city && country) { 
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""  
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter a valid value."  
+      }); 
+    }
   }
-
+  
   render() {
     return (
       <div>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,14 +1,16 @@
-import React from 'react';
+ import React from 'react';
 
 
 class Weather extends React.Component {
   render() {
     return (
       <div>
+        {/* display weather when values exist */}
         { this.props.city && this.props.country && <p>Location: { this.props.city }, { this.props.country }</p> }
         { this.props.temperature && <p>temperature: { this.props.temperature }</p> }
         { this.props.humidity && <p>Humidity: { this.props.humidity }</p> }
         { this.props.description && <p>Conditions: { this.props.description }</p> }  
+        { this.props.error && <p>{ this.props.error }</p> }  
       </div>
     );
   }
