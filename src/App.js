0a0a4d08e80f4aab7e042e867cfa99818131ca1c
James Hughes <james.a.hughes@gmail.com>
2018-08-01 15:46:54 -0500

    Add classNames to elements for styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
- import React from 'react';
+import React from 'react';
 import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
@@ -50,15 +50,28 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Titles />
-        <Form getWeather={this.getWeather} />
-        <Weather 
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
-          error={this.state.error} />
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather 
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error} 
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,15 +3,33 @@ import React from 'react';
 
 
 
-const Weather = props => (        // curly bracket and 'return' not needed since returning one element
-  <div>
-    {/* display weather when values exist */}
-    {/* NOTE: can't use 'this' when the component is not a Class */}
-    { props.city && props.country && <p>Location: { props.city }, { props.country }</p> }
-    { props.temperature && <p>temperature: { props.temperature }</p> }
-    { props.humidity && <p>Humidity: { props.humidity }</p> }
-    { props.description && <p>Conditions: { props.description }</p> }  
-    { props.error && <p>{ props.error }</p> }  
+const Weather = props => ( 
+  <div className="weather-info">
+    { 
+      props.city && props.country && <p className="weather__key">Location: 
+        <span className="weather__value"> { props.city }, { props.country }</span>
+      </p> 
+    }
+    { 
+      props.temperature && <p className="weather__key">temperature: 
+        <span className="weather__value"> { props.temperature }</span>
+      </p> 
+    }
+    { 
+      props.humidity && <p className="weather__key">Humidity: 
+        <span className="weather__value"> { props.humidity }</span>
+      </p> 
+    }
+    { 
+      props.description && <p className="w eather__key">Conditions: 
+        <span className="weather__value">  { props.description }</span>
+      </p> 
+    }  
+    { 
+      props.error && <p className="weather__error">
+        <span>{ props.error }</span>
+      </p> 
+    }  
   </div>
 );
 
